9351c9d5bfe1f107e862fa94982f7db2ca0b4a94
Fəqan <70229842+FeqanUmarov@users.noreply.github.com>
2020-09-03 01:06:53 +0400

    reverse_order

---
+++ reverse_order.py
@@ -0,0 +1,18 @@
+import Add_Book
+info = input("Her hansi bir ifade daxil edin:")
+
+info_list = list(info)
+
+
+info_list_len=-(len(info_list))
+
+sentence=[]
+
+m=-1
+
+for s in info_list:
+    if m>=info_list_len:
+        sentence.insert(m,s)
+        m+=-1
+for s in sentence:
+    print(s,end="")
